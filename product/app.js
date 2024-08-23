dc76df0f75f995ca9a679a7fcca5716902c23716
phanhuuthanh <thanhphan900068@gmail.com>
2024-08-23 07:09:04 -0700

    updata products page

---
+++ product/app.js
@@ -0,0 +1,185 @@
+let listProductHTML = document.querySelector(".listProduct");
+let listCartHTML = document.querySelector(".listCart");
+let iconCart = document.querySelector(".icon-cart");
+let iconCartSpan = document.querySelector(".icon-cart span");
+let body = document.querySelector("body");
+let closeCart = document.querySelector(".close");
+let cart = [];
+
+iconCart.addEventListener("click", () => {
+  body.classList.toggle("showCart");
+});
+closeCart.addEventListener("click", () => {
+  body.classList.toggle("showCart");
+});
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-analytics.js";
+import {
+  addDoc,
+  collection,
+  doc,
+  setDoc,
+  getDoc,
+  getDocs,
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA6t-mCTJG3nXau-LUJx5TbOcjqsJaCJyg",
+  authDomain: "phivutrieudo-f54c0.firebaseapp.com",
+  projectId: "phivutrieudo-f54c0",
+  storageBucket: "phivutrieudo-f54c0.appspot.com",
+  messagingSenderId: "693709761960",
+  appId: "1:693709761960:web:b7cabbb5ed93f2eccb019d",
+  measurementId: "G-D9PESBNNFZ",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+const products = collection(db, "products");
+
+const addDataToHTML = async () => {
+  const querySnapshot = await getDocs(products);
+  querySnapshot.forEach((doc) => {
+    let product = doc.data();
+    console.log(product);
+
+    document.getElementById("products").innerHTML += `
+    <li class="product-item">
+        <img src="${product.image}" alt="${product.title}" class="product-image">
+        <div class="product-details">
+          <div class="product-title">${product.title}</div>
+          <div class="product-description">${product.desc}</div>
+          <div class="product-price">${product.price}</div>
+        </div>
+    </li>
+        `;
+  });
+  // remove datas default from HTML
+
+  // add new data
+};
+listProductHTML.addEventListener("click", (event) => {
+  let positionClick = event.target;
+  if (positionClick.classList.contains("addCart")) {
+    let id_product = positionClick.parentElement.dataset.id;
+    addToCart(id_product);
+  }
+});
+const addToCart = (product_id) => {
+  let positionThisProductInCart = cart.findIndex(
+    (value) => value.product_id == product_id
+  );
+  if (cart.length <= 0) {
+    cart = [
+      {
+        product_id: product_id,
+        quantity: 1,
+      },
+    ];
+  } else if (positionThisProductInCart < 0) {
+    cart.push({
+      product_id: product_id,
+      quantity: 1,
+    });
+  } else {
+    cart[positionThisProductInCart].quantity =
+      cart[positionThisProductInCart].quantity + 1;
+  }
+  addCartToHTML();
+  addCartToMemory();
+};
+const addCartToMemory = () => {
+  localStorage.setItem("cart", JSON.stringify(cart));
+};
+const addCartToHTML = () => {
+  listCartHTML.innerHTML = "";
+  let totalQuantity = 0;
+  if (cart.length > 0) {
+    cart.forEach((item) => {
+      totalQuantity = totalQuantity + item.quantity;
+      let newItem = document.createElement("div");
+      newItem.classList.add("item");
+      newItem.dataset.id = item.product_id;
+
+      let positionProduct = products.findIndex(
+        (value) => value.id == item.product_id
+      );
+      let info = products[positionProduct];
+      listCartHTML.appendChild(newItem);
+      newItem.innerHTML = `
+            <div class="image">
+                    <img src="${info.image}">
+                </div>
+                <div class="name">
+                ${info.name}
+                </div>
+                <div class="totalPrice">$${info.price * item.quantity}</div>
+                <div class="quantity">
+                    <span class="minus"><</span>
+                    <span>${item.quantity}</span>
+                    <span class="plus">></span>
+                </div>
+            `;
+    });
+  }
+  iconCartSpan.innerText = totalQuantity;
+};
+
+listCartHTML.addEventListener("click", (event) => {
+  let positionClick = event.target;
+  if (
+    positionClick.classList.contains("minus") ||
+    positionClick.classList.contains("plus")
+  ) {
+    let product_id = positionClick.parentElement.parentElement.dataset.id;
+    let type = "minus";
+    if (positionClick.classList.contains("plus")) {
+      type = "plus";
+    }
+    changeQuantityCart(product_id, type);
+  }
+});
+const changeQuantityCart = (product_id, type) => {
+  let positionItemInCart = cart.findIndex(
+    (value) => value.product_id == product_id
+  );
+  if (positionItemInCart >= 0) {
+    let info = cart[positionItemInCart];
+    switch (type) {
+      case "plus":
+        cart[positionItemInCart].quantity =
+          cart[positionItemInCart].quantity + 1;
+        break;
+
+      default:
+        let changeQuantity = cart[positionItemInCart].quantity - 1;
+        if (changeQuantity > 0) {
+          cart[positionItemInCart].quantity = changeQuantity;
+        } else {
+          cart.splice(positionItemInCart, 1);
+        }
+        break;
+    }
+  }
+  addCartToHTML();
+  addCartToMemory();
+};
+
+const initApp = () => {
+  addDataToHTML();
+
+  // get data cart from memory
+  if (localStorage.getItem("cart")) {
+    cart = JSON.parse(localStorage.getItem("cart"));
+    addCartToHTML();
+  }
+};
+initApp();
